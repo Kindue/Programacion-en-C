ff195eadb78382f6288eb1ea956b0a30b9f03ff1
Agustin Villamil <villa.09.agus@gmail.com>
2023-09-26 07:15:45 -0300

    TP2 casi finalizado

--- TPn2/main.c
+++ TPn2/main.c
@@ -72,16 +72,99 @@
 
 int main()
 {
-    int arreglo[8] = {20,5,6,7,5,2,8,2};
-    int tamanio = sizeof(arreglo) / sizeof(arreglo[0]);
-    int longitud = 5;
-    int *pArr = arreglo;
-    inicializar(pArr, longitud - 1);
-    for (int i = 0; i < tamanio; i++) {
-        printf("%d ", arreglo[i]);
-    }
 
+//  EJERCICIO 5
+
+//    int arreglo1[30];
+//    int arreglo2[20];
+//    int tamanio1 = sizeof(arreglo1) / sizeof(arreglo1[0]);
+//    int tamanio2 = sizeof(arreglo2) / sizeof(arreglo2[0]);
+//    int *pArr1 = arreglo1;
+//    int *pArr2 = arreglo2;
+//    int *pArr3;
+//
+//
+//    inicializar(pArr1, tamanio1);
+//    printf("Arreglo 1 inicializado con las siguientes componentes:\n");
+//    for (int i = 0; i < tamanio1; i++) {
+//        printf("%d ", arreglo1[i]);
+//    }
+//    printf("\n");
+//
+//
+//    printf("\n");
+//    printf("Arreglo 1 con las primeras %d componentes ordenadas:\n", tamanio1);
+//    ordenarAscendente(pArr1, tamanio1);
+//    for (int i = 0; i < tamanio1; i++) {
+//        printf("%d ", arreglo1[i]);
+//    }
+//    printf("\n");
+//
+//
+//    printf("\n");
+//    if(estanOrdenados(pArr1, tamanio1)){
+//        printf("El arreglo 1 hasta la componente %d esta ordenado", tamanio1);
+//    }else{
+//        printf("El arreglo 1 hasta la componente %d NO esta ordenado", tamanio1);}
+//    printf("\n");
+//
+//
+//    printf("\n");
+//    printf("Arreglo 2 con las primeras %d componentes ordenadas:\n", tamanio2);
+//    inicializar(pArr2, tamanio2);
+//    ordenarAscendente(pArr2, tamanio2);
+//    for (int i = 0; i < tamanio2; i++) {
+//        printf("%d ", arreglo2[i]);
+//    }
+//    printf("\n");
+//
+//
+//    printf("\n");
+//    int tamanio3 = tamanio2 + tamanio1;
+//    pArr3 = intercalarArreglos(*pArr1, *pArr2, tamanio1, tamanio2);
+//    printf("Nuevo arreglo formado al intercalar el arreglo 1 y el arreglo 2:\n");
+//    for (int i = 0; i < tamanio3; i++) {
+//        printf("%d ", pArr3[i]);
+//    }
+
+// EJERCICIO 6
+
+//    int arreglo[5] = {1,2,3,4,5};
+//    int *pArreglo = arreglo;
+//    int tamanio = sizeof(arreglo) / sizeof(arreglo[0]);
+//    printf("Arreglo sin cambios:\n");
+//    for (int i = 0; i < tamanio; i++) {
+//        printf("%d ", pArreglo[i]);
+//    }
+//    printf("\n");
+//    reemplazar(arreglo, 3, 0);
+//    printf("Arreglo luego de cambiar con el arreglo directamente:\n");
+//    for (int i = 0; i < tamanio; i++) {
+//        printf("%d ", pArreglo[i]);
+//    }
+//    printf("\n");
+//    reemplazar(pArreglo, 3, 0, tamanio);
+//    printf("Arreglo luego de volver a cambiar de 0 a 3 con un puntero hacia el arreglo:\n");
+//    for (int i = 0; i < tamanio; i++) {
+//        printf("%d ", pArreglo[i]);
+//    }
+
+//  EJERCICIO 7
+
+
+    int longitud = 3;
+    int arr_num [longitud];
+    int * arr_punt [longitud];
+// INICIALIZACION
+    int i = 0;
+    for(i=0; i<longitud; i++){
+        arr_num[i] = i+1;
+        arr_punt[i] = &arr_num[i];
+        }
+    // INSTRUCCION 1
+    invertir_arr_num(arr_num, longitud);
+    // INSTRUCCION 2
+    invertir_arr_punt(arr_punt, longitud);
 
     return 0;
 }
-
--- TPn2/resolucion.c
+++ TPn2/resolucion.c
@@ -1,20 +1,143 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 //Dise˜nar funciones o procedimientos para implementar las siguientes operaciones sobre
-//arreglos de n´umeros enteros:
+//arreglos de numeros enteros:
 //a) Un procedimiento que inicialice al azar los primeros n elementos de un arreglo reci-
 //bido como argumento.
 //b) Un procedimiento que ordene en forma ascendente los primeros n elementos de un
 //arreglo recibido como argumento.
-//c) Una funci´on que determine si los primeros n elementos de un arreglo recibido como
+//c) Una funcion que determine si los primeros n elementos de un arreglo recibido como
 //argumento se encuentran ordenados de forma ascendente.
 //d ) Un procedimiento que reciba dos arreglos de enteros A1 y A2, cada uno con una
 //cierta cantidad de elementos previamente ordenados de forma ascendente, y que
-//retorne un nuevo arreglo resultante de intercalar de forma ordenada y sin repetici´on
+//retorne un nuevo arreglo resultante de intercalar de forma ordenada y sin repeticion
 //los elementos de A1 y A2.
 
-void inicializar(int* pArreglo, int longitud){
-    for(int i = 0; i < longitud; i++){
-        *(pArreglo + i) = rand();
+void inicializar(int* pArreglo, int cantidad){
+    srand(time(NULL));
+    for(int i = 0; i < cantidad; i++){
+        int r = rand();
+        pArreglo[i] = r % 1000;
     }
 }
+
+void cambio(int* ele1, int* ele2){
+    int temp = *ele1;
+    *ele1 = *ele2;
+    *ele2 = temp;
+}
+void ordenarAscendente(int* pArreglo, int cantidad){
+    int i, j;
+    int cambiado;
+    for(j = 0; j < cantidad - 1; j++){
+        cambiado = 0;
+        for(i = 0; i < cantidad - j - 1; i++){
+            if(pArreglo[i] > pArreglo[i + 1]){
+                cambio(&pArreglo[i], &pArreglo[i+1]);
+                cambiado = 1;
+            }
+        }
+        if(cambiado == 0){
+            break;
+        }
+    }
+}
+
+int estanOrdenados(int* pArreglo, int cantidad){
+    int i;
+    int ordenados = 1;
+    for(i = 0; i < cantidad - 1; i++){
+        if(pArreglo[i] > pArreglo[i + 1]){
+            ordenados = 0;
+        }
+        if(ordenados == 0){
+            break;
+        }
+    }
+    return ordenados;
+}
+
+int* intercalarArreglos(int* pArr1, int* pArr2, int ele1, int ele2){
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int nuevoArr[ele1 + ele2];
+    int *aRet = nuevoArr;
+    while((i < ele1) && (j < ele2)){
+        if(pArr1[i] < pArr2[j]){
+            nuevoArr[k] = pArr1[i];
+            i++;
+        }else{
+            if(pArr2[j] < pArr1[i]){
+                nuevoArr[k] = pArr2[j];
+                j++;
+            }else{
+                nuevoArr[k] = pArr1[i];
+                i++;
+                j++;
+            }
+        }
+        k++;
+    }
+    while(i < ele1){
+        nuevoArr[k] = pArr1[i];
+        i++;
+    }
+    while(j < ele1){
+        nuevoArr[k] = pArr1[j];
+        j++;
+    }
+    return aRet;
+}
+
+//Implemente un procedimiento que, dado un arreglo de enteros arr y dos enteros a y n,
+//modifique el arreglo arr de forma tal que cada aparicion del entero a sea reemplazada por
+//el entero n. Analice dos variantes, una en funcion de cada uno de los siguientes prototipos:
+//void reemplazar(int arr [], int a, int n);
+//void reemplazar(int * arr, int a, int n, int size);
+//Compruebe el correcto funcionamiento de las dos variantes considerando reemplazar el
+//entero 3 por el entero 0 en un arreglo inicializado con los valores 1,2,3,4,5.
+
+
+void reemplazar(int arr[], int a, int n){
+    int tamanio = sizeof(arr)/sizeof(arr[0]);
+    int m = sizeof(arr);
+    int k = sizeof(arr[0]);
+    int p;
+    for(int i = 0; i < tamanio; i++){
+        p = arr[i];
+        if(arr[i] == a){
+            arr[i] = n;
+        }
+    }
+}
+
+//void reemplazar(int* arr, int a, int n, int size){
+//    for(int i = 0; i < size; i++){
+//        if(arr[i] == a){
+//            *(arr + i) = n;
+//        }
+//    }
+//}
+
+void invertir_arr_num(int arr [], int size){
+        int f = 0, c = size-1;
+        int aux;
+        while(f < c){
+            aux = arr[f];
+            arr[f++] = arr[c];
+            arr[c--] = aux;
+            }
+        }
+void invertir_arr_punt(int ** arr, int size){
+    int ** cola = arr + size - 1;
+    int * aux;
+    while(arr != cola){
+        aux = *arr;
+        *arr = *cola;
+        *cola = aux;
+        if ((++arr)!=cola)
+            cola--;
+        }
+    }
