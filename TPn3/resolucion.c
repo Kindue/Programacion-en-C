ff15a49e5ae96429cd1413cbd2c2a2f0334baa82
Agustin Villamil <villa.09.agus@gmail.com>
2023-09-28 07:25:48 -0300

    TP3 Inicializado

---
+++ TPn3/resolucion.c
@@ -0,0 +1,12 @@
+//Considerar los siguientes planteos en el contexto del lenguaje C:
+//a) ¿Que diferencia una estructura de datos estatica de una dinamica? Encontrar al
+//menos dos ejemplos de cada categorıa.
+//b) ¿Una variable de tipo puntero en s´ı misma, constituye una estructura de datos est´ati-
+//ca o dinamica?
+//c) ¿Cual es la utilidad de las funciones de librerıa malloc() y free()?
+//d ) ¿Como hace el compilador para saber cuanta memoria debe reservar en el HEAP ante
+//cada invocacion a la funcion malloc()?
+//e) Analice las diferencias que presentan las datos almacenados en STACK, frente aquellos
+//que son almacenados en HEAP. Tenga en cuenta las siguientes caracterısticas:
+//Alcance de los datos.
+//Persistencia de los datos: temporal, permanente.
